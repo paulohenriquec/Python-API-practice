8b2d430e8c094c1735fd348ac53a428f597e4049
paulohenriquec <paul.cavalcantesilva@gmail.com>
2026-01-19 20:51:40 -0300

    Implementei uma leitura de ceps de uma fonte externa csv para ffazer a requisição e pegar as informações de cada cep

--- cep.py
+++ cep.py
@@ -6,6 +6,7 @@ import json
 from tqdm import tqdm
 
 data = []
+fonte_externa = dict()
 
 #%%
 #2ª etapa: realizar requisição dinâmica
@@ -26,5 +27,30 @@ while True:
         data.append(resp.json())
 #%%
 #3ª etapa: salvar em um arquivo json
+# estou abrindo um arquivo chamado ceps.json no modelo de escrita com a codificação utf-8 e estou salvando os dados de data, nesse arquivo
 with open("ceps.json", "w", encoding='utf-8') as open_file:
     json.dump(data, open_file, ensure_ascii=False, indent=4)
+#%%
+#4ª etapa: conversão em dataframe e csv
+df = pd.DataFrame(data)
+df.to_csv("ceps.csv", sep=";")
+
+#%%
+#5ª etapa: leitura de ceps de uma fonte externa
+with open("Tabela de CEPs Brasileiros por Região.csv", "r") as open_file:
+    lines = open_file.readlines()
+
+keys = lines[0].strip("\n").split(",")
+
+for k in keys:
+    fonte_externa[k] = []
+
+for l in lines[1:]:
+    value = l.strip("\n").split(",")
+
+    for i in range(len(value)):
+        fonte_externa[keys[i]].append(value[i])
+
+print(fonte_externa)
+
+##PRÓXIMO PASSO, PEGAR OS CEPS DESSA LISTA E FAZER A REQUISIÇÃO
