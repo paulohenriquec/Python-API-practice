c9131e98c71f3f9b217f48033cac50a07cff9831
paulohenriquec <paul.cavalcantesilva@gmail.com>
2026-01-19 20:28:10 -0300

    Melhoria na requisição e adição dos retornos em uma lista

--- cep.py
+++ cep.py
@@ -5,12 +5,22 @@ import pandas as pd
 import json
 from tqdm import tqdm
 
+data = []
+
 #%%
 #2ª etapa: realizar requisição dinâmica
-cep = input("Digite qual CEP você deseja pesquisar: ")
+while True:
+    #Pego a entrada do usuário
+    cep = input("Digite qual CEP você deseja pesquisar: ")
 
-url = "https://viacep.com.br/ws/{cep}/json/"
+    #Verifico se ele quer encerrar a busca
+    if cep == "sair" or cep == "":
+        break
+    
+    #Faço a requisição
+    url = "https://viacep.com.br/ws/{cep}/json/"
+    resp = requests.get(url.format(cep=cep))
 
-resp = requests.get(url.format(cep=cep))
-
-print(resp)
+    #Se a requisição tiver sido bem sucedida, eu salvo o retorno json na minha lista data
+    if resp.status_code == 200:
+        data.append(resp.json())
